e5019f1a605bc6e29105bbe9f6771925739e5f1a
isharamygina <121282432+isharamygina@users.noreply.github.com>
2024-02-24 00:00:15 +0300

    Add files via upload

---
+++ src/monom.cpp
@@ -0,0 +1,72 @@
+#pragma once
+#include "monom.h"
+using namespace std;
+Monom::Monom()
+{
+    next = nullptr;
+    k = 0;
+    degree = 0;
+}
+Monom::Monom(Monom& m)
+{
+    k = m.k;
+    degree = m.degree;
+    next = nullptr;
+}
+Monom::Monom(int deg)
+{
+    if (deg > 1000 || deg < 0)
+        throw out_of_range("Degree cant be more than 1000 or less than 0");
+    next = nullptr;
+    k = 0;
+    degree = deg;
+}
+Monom::Monom(double k_, int degree_) {
+    if (degree_ > 999 || degree_ < 0)
+        throw out_of_range("Degree cant be more than 999 or less than 0");
+    k = k_;
+    degree = degree_;
+    next = nullptr;
+}
+bool Monom::operator==(const Monom& m) const noexcept
+{
+    return ((k == m.k) && (abs(degree - m.degree) < 1e-15));
+}
+bool Monom::operator!=(const Monom& m) const noexcept
+{
+    return !(*this == m);
+}
+void Monom::check(const Monom& s)
+{
+    int x1 = degree / 100;
+    int x2 = s.degree / 100;
+    if (x1 + x2 > 9)
+        throw out_of_range("The degree of x in result shouldnt be more than 9");
+    int y1 = degree / 10 - x1 * 10;
+    int y2 = s.degree / 10 - x2 * 10;
+    if (y1 + y2 > 9)
+        throw out_of_range("The degree of y in result shouldnt be more than 9");
+    int z1 = degree - x1 * 100 - y1 * 10;
+    int z2 = s.degree - x2 * 100 - y2 * 10;
+    if (z1 + z2 > 9)
+        throw out_of_range("The degree of z in result shouldnt be more than 9");
+}
+Monom  Monom::operator*(const Monom& n)
+{
+    check(n);
+    Monom res;
+    res.k = k * n.k;
+    res.degree = degree + n.degree;
+    return res;
+}
+Monom::~Monom() {
+    k = 0;
+    degree = 0;
+    next = nullptr;
+}
+Monom& Monom::operator=(const Monom& m)
+{
+    degree = m.degree;
+    k = m.k;
+    return *this;
+}
