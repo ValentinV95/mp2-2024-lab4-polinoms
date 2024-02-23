ba8030247b26d733278d10e6d70769adeae12466
isharamygina <121282432+isharamygina@users.noreply.github.com>
2024-02-23 23:59:06 +0300

    Add files via upload

--- include/monom.h
+++ include/monom.h
@@ -3,79 +3,22 @@
 
 using namespace std;
 
+
 class Monom {
+    friend class Polinom;
+    Monom(int deg);
 public:
     double k;
     int degree;
     Monom* next;
 
-    Monom()
-    {
-        next = nullptr;
-        k = 0;
-        degree = 0;
-    }
-    Monom(Monom& m)
-    {
-        k = m.k;
-        degree = m.degree;
-        next = nullptr;
-    }
-    Monom(int deg)
-    {
-        if (deg > 1000 || deg < 0)
-            throw out_of_range("Degree cant be more than 1000 or less than 0");
-        next = nullptr;
-        k = 0;
-        degree = deg;
-    }
-    Monom(double k_, int degree_) {
-        if (degree_ > 1000 || degree_ < 0)
-            throw out_of_range("Degree cant be more than 1000 or less than 0");
-        k = k_;
-        degree = degree_;
-        next = nullptr;
-    }
-    bool operator==(const Monom& m) const noexcept
-    {
-        return ((k == m.k) && (abs(degree - m.degree)<1e-15));
-    }
-    bool operator!=(const Monom& m) const noexcept
-    {
-        return !(*this == m);
-    }
-    void check(const Monom& s)
-    {
-        int x1 = degree / 100;
-        int x2 = s.degree / 100;
-        if (x1 + x2 > 9)
-            throw out_of_range("The degree of x in result shouldnt be more than 9");
-        int y1 = degree / 10 - x1 * 10;
-        int y2 = s.degree / 10 - x2 * 10;
-        if (y1 + y2 > 9)
-            throw out_of_range("The degree of y in result shouldnt be more than 9");
-        int z1 = degree - x1 * 100 - y1 * 10;
-        int z2 = s.degree - x2 * 100 - y2 * 10;
-        if (z1 + z2 > 9)
-            throw out_of_range("The degree of z in result shouldnt be more than 9");
-    }
-    Monom  operator*(const Monom& n)
-    {
-        check(n);
-        Monom res;
-        res.k = k * n.k;
-        res.degree = degree + n.degree;
-        return res;
-    }
-    ~Monom() {
-        k = 0;
-        degree = 0;
-        next = nullptr;
-    }
-    Monom operator=(const Monom& m)
-    {
-        degree = m.degree;
-        k = m.k;
-        return *this;
-    }
+    Monom();
+    Monom(Monom& m);
+    Monom(double k_, int degree_);
+    bool operator==(const Monom& m) const noexcept;
+    bool operator!=(const Monom& m) const noexcept;
+    void check(const Monom& s);
+    Monom  operator*(const Monom& n);
+    ~Monom();
+    Monom& operator=(const Monom& m);
 };
--- include/polinom.h
+++ include/polinom.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Monom.h"
 #include <math.h>
+using namespace std;
 
 class Polinom
 {
@@ -8,241 +9,24 @@ class Polinom
     Monom* last;
 public:
     
-    Polinom(const Polinom& p)
-    {
-        head = new Monom(1000);
-        last = head;
-        Monom* cur = p.head;
-        while (cur != p.last)
-        {
-            cur = cur->next;
-            push(*cur);
-        }
-    }
-    Polinom()
-    {
-        head = new Monom(1000);
-        last = head;
-    }
-    void addMonom(double k, int deg) {
-        Monom* p = new Monom(k, deg);
-        addMonom(*p);
-    }
-    void addMonom(Monom& p) {
-        Monom* cur = head;
-        while (cur != last && p.degree < cur->next->degree)//to find right place 
-        {
-            cur = cur->next;
-        }
-        if (cur != last && p.degree > cur->next->degree)//new monom to the middle
-        {
-            Monom* tmp = cur->next;
-            cur->next = new Monom(p);
-            cur = cur->next;
-            cur->next = tmp;
-            if (tmp->next == nullptr)
-                last = tmp;
-        }
-        else 
-        {
-            if (cur != last)
-                cur = cur->next;
-            if (p.degree == cur->degree)//sum monoms with the same degree
-            {
-                cur->k += p.k;
-                if (abs(cur->k) < 1e-15)
-                    deleteMonom(cur);
-            }
-            else//new monom to the end
-            {
-                push(p);
-            }
-        }
-
-    }
-    void deleteMonom(Monom* m)
-    {
-        if (head == m)
-            throw logic_error("You cant delete the head as a monom");
-        if (head == last)
-            throw logic_error("Your polinom is empty");
-        Monom* pre_cur=head;
-        Monom* cur = head->next;
-        while (cur != m)
-        {
-            pre_cur = pre_cur->next;
-            cur = cur->next;
-        }
-        pre_cur->next = cur->next;
-if (pre_cur->next == nullptr)
-            last = pre_cur;
-        delete cur;
-        
-    }
-    void push(Monom& p)
-    {
-        last->next = new Monom(p);
-        last = last->next;
-    }
-    Polinom operator+(const Polinom& P)
-    {
-        Polinom res;
-        Monom* cur1 = head->next;
-        Monom* cur2 = P.head->next;
-        while (cur1 != nullptr && cur2 != nullptr)
-        {
-            if (cur1->degree > cur2->degree)
-            {
-                res.push(*cur1);
-                cur1 = cur1->next;
-            }
-            else if (cur1->degree < cur2->degree)
-            {
-                res.push(*cur2);
-                cur2 = cur2->next;
-            }
-            else if (cur1->degree == cur2->degree)
-            {
-                Monom tmp(*cur1);
-                tmp.k =(cur1->k) + (cur2->k);
-                if (abs(tmp.k) >= 1e-15)
-                    res.push(tmp);
-                cur1 = cur1->next;
-                cur2 = cur2->next;
-            }
-        }
-        while (cur1 != nullptr)
-        {
-            res.push(*cur1);
-            cur1 = cur1->next;
-        }
-        while (cur2!=nullptr)
-        {
-            res.push(*cur2);
-            cur2 = cur2->next;
-        }
-        return res;
-    }
-    Polinom operator-(const Polinom& P)
-    {
-        Polinom res;
-        Monom* cur1 = head->next;
-        Monom* cur2 = P.head->next;
-        while (cur1 != nullptr && cur2 != nullptr)
-        {
-            if (cur1->degree > cur2->degree)
-            {
-                res.push(*cur1);
-                cur1 = cur1->next;
-            }
-            else if (cur1->degree < cur2->degree)
-            {
-                cur2->k *= -1;
-                res.push(*cur2);
-                cur2 = cur2->next;
-            }
-            else if (cur1->degree == cur2->degree)
-            {
-                Monom tmp(*cur1);
-                tmp.k = (cur1->k) - (cur2->k);
-                if (abs(tmp.k) >= 1e-15)
-                    res.push(tmp);
-                cur1 = cur1->next;
-                cur2 = cur2->next;
-            }
-        }
-        while (cur1 != nullptr)
-        {
-            res.push(*cur1);
-            cur1 = cur1->next;
-        }
-        while (cur2 != nullptr)
-        {
-            cur2->k *= -1;
-            res.push(*cur2);
-            cur2 = cur2->next;
-        }
-        return res;
-    }
-    Polinom operator*(const double a)
-    {
-        Polinom res(*this);
-        Monom* cur = res.head->next;
-        while (cur != nullptr)
-        {
-            cur->k = cur->k * a; 
-            if (abs(cur->k) < 1e-15)
-                deleteMonom(cur);
-            cur = cur->next;
-        }
-        return res;
-    }
-    Polinom operator*(const Monom& m)
-    {
-        Monom* cur = head;
-        Polinom res;
-        while (cur != last)
-        {
-            cur = cur->next;
-            if (abs((*cur * m).k)>=1e-15)
-                res.push(*cur * m);
-        }
-        return res;
-    }
-    Polinom operator*(const Polinom& P)
-    {
-        Polinom res;
-        Monom* cur = head;
-        while (cur != last)
-        {
-            Polinom tmp(P);
-            cur = cur->next;
-            res = res + (tmp * (*cur));
-        }
-        return res;
-    }
-    bool operator==(const Polinom& P) const 
-    {
-        Monom* cur = head->next;
-        Monom* cur2 = P.head->next;
-        while (cur != nullptr && cur2 != nullptr)
-        {
-            if (*cur != *cur2)
-            {
-                return false;
-                break;
-            }
-            cur = cur->next;
-            cur2 = cur2->next;
-        }
-        return (cur == nullptr && cur2 == nullptr);
-    }
-    bool operator!=(const Polinom& P) const 
-    {
-        return !(*this == P);
-    }
-    Polinom& operator=(Polinom& p)
-    {
-        if (this != &p)
-        {
-            Polinom tmp(p);
-            swapPolinoms(tmp);
-        }
-        return *this;
-    }
-    void swapPolinoms(Polinom& s) noexcept
-    {
-        swap(this->head, s.head);
-        swap(this->last, s.last);
-    }
-    const Monom* getHead() const noexcept
-    {
-        return head->next;
-    }
-    const Monom* getEnd() const noexcept
-    {
-        return last;
-    }
+    Polinom(const Polinom& p);
+    Polinom();
+    void addMonom(double k, int deg);
+    void addMonom(Monom& p);
+    void deleteMonom(Monom* m);
+    void push(Monom& p);
+    Polinom& operator=(const Polinom& p);
+    Polinom operator+(const Polinom& P);
+    Polinom operator-(const Polinom& P);
+    Polinom operator*(const double a);
+    Polinom operator*(const Monom& m);
+    Polinom operator*(const Polinom& P);
+    bool operator==(const Polinom& P) const;
+    bool operator!=(const Polinom& P) const;
+    void swapPolinoms(Polinom& s) noexcept;
+    const Monom* getHead() const noexcept;
+    const Monom* getEnd() const noexcept;
+    ~Polinom();
     friend istream& operator>>(istream& istr, Polinom& p)
     {
         Polinom newp;
@@ -311,13 +95,4 @@ if (pre_cur->next == nullptr)
         return ostr;
     }
 
-    ~Polinom() {
-        Monom* tmp = head;
-        while (head) {
-            head = head->next;
-            delete tmp;
-            tmp = head;
-        }
-        last = nullptr;
-    }
 };
