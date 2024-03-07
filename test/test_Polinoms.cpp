4afc7816f3e8ec6f7bec0942f8f0406fb05f1ba0
Petr Zaznobin <69428320+Petr-Zaznobin@users.noreply.github.com>
2024-03-07 21:18:31 +0300

    Add files via upload

--- test/test_Polinoms.cpp
+++ test/test_Polinoms.cpp
@@ -1,167 +1,320 @@
-#include "List.h"
+#include "Polinoms.h"
+
 #include "gtest.h"
 
-TEST(List, can_create_list)
+TEST(monom, can_create_monom)
 {
-    ASSERT_NO_THROW(List<int> l);
+    ASSERT_NO_THROW(monom mn(100, 2.0));
 }
 
-TEST(List, can_push_back_Node)
+TEST(monom, can_correctly_get_degree)
 {
-    List<int> l;
-    ASSERT_NO_THROW(l.push_back(3));
+    monom mn(100, 2.0);
+    EXPECT_EQ(mn.getDeg(), 100);
 }
 
-TEST(List, can_get_next_node)
+TEST(monom, can_correctly_get_coef)
 {
-    List<int> l;
-    l.push_back(3);
-    ASSERT_NO_THROW(l.getNext(0));
+    monom mn(100, -2.0);
+    EXPECT_EQ(mn.getCoef(), -2.0);
 }
 
-TEST(List, can_get_data)
+TEST(monom, can_compare_monom_false)
 {
-    List<int> l;
-    l.push_back(3);
-    ASSERT_NO_THROW(l.getData(1));
+    monom mn1(100, 2.0);
+    monom mn2(56, 3.2);
+    EXPECT_EQ(mn2 == mn1, 0);
 }
 
-TEST(List, can_correctly_get_data)
+TEST(monom, can_compare_monom_true)
 {
-    List<int> l;
-    l.push_back(3);
-    l.push_back(4);
-    EXPECT_EQ(l.getData(1), 3);
-    EXPECT_EQ(l.getData(2), 4);
+    monom mn1(100, 2.0);
+    monom mn2(100, 2.0);
+    EXPECT_EQ(mn2 == mn1, 1);
 }
 
-TEST(List, can_insert)
+TEST(monom, can_not_compare_monom_true)
 {
-    List<int> l;
-    l.push_back(3);
-    l.push_back(5);
-    ASSERT_NO_THROW(l.insert(1, 4));
+    monom mn1(100, 2.0);
+    monom mn2(56, 3.2);
+    EXPECT_EQ(mn2 != mn1, 1);
 }
 
-TEST(List, can_correctly_insert)
+TEST(monom, can_not_compare_monom_flase)
 {
-    List<int> l;
-    l.push_back(3);
-    l.push_back(5);
-    l.insert(1, 4);
-    EXPECT_EQ(l.getData(1), 4);
+    monom mn1(100, 2.0);
+    monom mn2(100, 2.0);
+    EXPECT_EQ(mn2 != mn1, 0);
 }
 
-TEST(List, can_equalize)
+TEST(monom, can_equalize_monom)
 {
-    List<int> l1;
-    List<int> l2;
-    l1.push_back(3);
-    l1.push_back(5);
-    ASSERT_NO_THROW(l2 = l1);
+    monom mn1(100, 2.0);
+    monom mn2(56, 3.2);
+    ASSERT_NO_THROW(mn2 = mn1);
 }
 
-TEST(List, can_correctly_equalize)
+TEST(monom, can__correctly_equalize_monom)
 {
-    List<int> l1;
-    List<int> l2;
-    l1.push_back(3);
-    l1.push_back(5);
-    l2 = l1;
-    EXPECT_EQ(l2.getData(1), 3);
-    EXPECT_EQ(l2.getData(2), 5);
+    monom mn1(100, 2.0);
+    monom mn2(56, 3.2);
+    mn2 = mn1;
+    EXPECT_EQ(mn2, mn1);
 }
 
-TEST(List, can_pop)
+TEST(Polinom, can_create_polinom)
 {
-    List<int> l;
-    l.push_back(3);
-    l.push_back(4);
-    ASSERT_NO_THROW(l.pop());
+    ASSERT_NO_THROW(Polinom p(1));
 }
 
-TEST(List, can_pop_correctly)
+TEST(Polinom, can_correctly_get_size)
 {
-    List<int> l;
-    l.push_back(3);
-    l.push_back(4);
-    l.pop();
-    ASSERT_ANY_THROW(l.getNext(2));
+    Polinom p(1);
+    EXPECT_EQ(1, p.get_size());
 }
 
-TEST(List, can_compare_correctly_true)
+TEST(Polinom, can_add_monom)
 {
-    List<int> l1;
-    List<int> l2;
-    l1.push_back(3);
-    l1.push_back(4);
-    l2.push_back(3);
-    l2.push_back(4);
-    EXPECT_EQ(l1==l2, 1);
+    Polinom p(0);
+    monom mn(1, 1);
+    ASSERT_NO_THROW(p.addMonom(mn));
 }
 
-TEST(List, can_compare_correctly_false)
+TEST(Polinom, can_get_monom)
 {
-    List<int> l1;
-    List<int> l2;
-    l1.push_back(3);
-    l1.push_back(4);
-    l2.push_back(3);
-    EXPECT_EQ(l1==l2, 0);
-    l2.push_back(5);
-    EXPECT_EQ(l1==l2, 0);
+    Polinom p(0);
+    monom mn(1, 1);
+    p.addMonom(mn);
+    ASSERT_NO_THROW(p.get_monom(1));
 }
 
-TEST(List, can_not_compare_correctly_false)
+TEST(Polinom, can_correctly_get_monom)
 {
-    List<int> l1;
-    List<int> l2;
-    l1.push_back(3);
-    l1.push_back(4);
-    l2.push_back(3);
-    l2.push_back(4);
-    EXPECT_EQ(l1!=l2, 0);
+    Polinom p(0);
+    monom mn(1, 1);
+    p.addMonom(mn);
+    EXPECT_EQ(p.get_monom(1), mn);
 }
 
-TEST(List, can_not_compare_correctly_true)
+TEST(Polinom, can_correctly_delete_monom_1)
 {
-    List<int> l1;
-    List<int> l2;
-    l1.push_back(3);
-    l1.push_back(4);
-    l2.push_back(3);
-    EXPECT_EQ(l1!=l2, 1);
-    l2.push_back(5);
-    EXPECT_EQ(l1!=l2, 1);
+    Polinom p(0);
+    monom mn(1, 1);
+    monom mn2(2, 1);
+    monom mn3(3, 1);
+    p.addMonom(mn);
+    p.addMonom(mn2);
+    p.addMonom(mn3);
+    p.delMonom(1);
+    EXPECT_EQ(p.get_monom(1).getDeg(), mn2.getDeg());
 }
 
-TEST(List, can_delete)
+TEST(Polinom, can_correctly_delete_monom_2)
 {
-    List<int> l1;
-    l1.push_back(3);
-    l1.push_back(4);
-    l1.push_back(5);
-    ASSERT_NO_THROW(l1.del(1));
+    Polinom p(0);
+    monom mn(1, 1);
+    monom mn2(2, 1);
+    monom mn3(3, 1);
+    p.addMonom(mn);
+    p.addMonom(mn2);
+    p.addMonom(mn3);
+    p.delMonom(2);
+    EXPECT_EQ(p.get_monom(2).getDeg(), mn3.getDeg());
 }
 
-TEST(List, can_delete_correctly_1)
+TEST(Polinom, can_correctly_delete_monom_3)
 {
-    List<int> l1;
-    l1.push_back(3);
-    l1.push_back(4);
-    l1.push_back(5);
-    l1.del(1);
-    EXPECT_EQ(l1.getData(1), 4);
+    Polinom p(0);
+    monom mn(1, 1);
+    monom mn2(2, 1);
+    monom mn3(3, 1);
+    p.addMonom(mn);
+    p.addMonom(mn2);
+    p.addMonom(mn3);
+    p.delMonom(3);
+    ASSERT_ANY_THROW(p.get_monom(3));
+}
+
+TEST(Polinom, can_correctly_equalize_polinoms_true) {
+    Polinom p(0);
+    Polinom p1(0);
+    monom mn1(1, 2);
+    monom mn2(2, 2);
+    p.addMonom(mn1);
+    p.addMonom(mn2);
+    p1.addMonom(mn1);
+    p1.addMonom(mn2);
+    EXPECT_EQ(p == p1, true);
+}
+
+TEST(Polinom, can_correctly_equalize_polinoms_false) {
+    Polinom p(0);
+    Polinom p1(0);
+    monom mn1(1, 2);
+    monom mn2(2, 2);
+    p.addMonom(mn1);
+    p.addMonom(mn1);
+    p1.addMonom(mn2);
+    p1.addMonom(mn2);
+    EXPECT_EQ(p == p1, false);
+}
+
+TEST(Polinom, can_correctly_equate) {
+    Polinom p(0);
+    monom mn1(1, 2);
+    monom mn2(2, 2);
+    p.addMonom(mn1);
+    p.addMonom(mn2);
+    Polinom p1(0);
+    p1 = p;
+    EXPECT_EQ(p1.get_monom(2).getDeg(), 2);
+    EXPECT_EQ(p1.get_monom(1).getDeg(), 1);
+
+}
+
+TEST(Polinom, can_adding_polinoms_correctly)
+{
+    Polinom p(0);
+    Polinom p1(0);
+    monom mn1(1, 3);
+    monom mn2(2, 2);
+    monom mn3(1, 6);
+    monom mn4(2, 4);
+    p.addMonom(mn1);
+    p.addMonom(mn2);
+    p1.addMonom(mn1);
+    p1.addMonom(mn2);
+    Polinom res(0);
+    res.addMonom(mn3);
+    res.addMonom(mn4);
+    Polinom ad(0);
+    ad = p + p1;
+    EXPECT_EQ(ad, res);
 }
 
 
-TEST(List, can_delete_correctly_2)
+TEST(Polinom, can_adding_polinoms_correctly_with_diff_degs)
 {
-    List<int> l1;
-    l1.push_back(3);
-    l1.push_back(4);
-    l1.push_back(5);
-    l1.del(3);
-    ASSERT_ANY_THROW(l1.getData(3));
+    Polinom p(0);
+    Polinom p1(0);
+    monom mn1(1, 3);
+    monom mn2(2, 2);
+    monom mn3(3, 1);
+    p.addMonom(mn1);
+    p.addMonom(mn2);
+    p1.addMonom(mn3);
+    Polinom res(0);
+    res.addMonom(mn1);
+    res.addMonom(mn2);
+    res.addMonom(mn3);
+    Polinom ad(0);
+    ad = p + p1;
+    EXPECT_EQ(ad, res);
 }
+
+TEST(Polinom, can_sub_polinoms_correctly)
+{
+    Polinom p(0);
+    Polinom p1(0);
+    monom mn1(1, 3);
+    monom mn2(2, 2);
+    monom mn3(1, 2);
+    monom mn4(2, 1);
+    monom mn5(1, 1);
+    monom mn6(2, 1);
+    p.addMonom(mn1);
+    p.addMonom(mn2);
+    p1.addMonom(mn3);
+    p1.addMonom(mn4);
+    Polinom res(0);
+    res.addMonom(mn5);
+    res.addMonom(mn6);
+    Polinom su(0);
+    su = p - p1;
+    EXPECT_EQ(su, res);
+}
+
+TEST(Polinom, can_sub_polinoms_correctly_with_diff_degs)
+{
+    Polinom p(0);
+    Polinom p1(0);
+    monom mn1(1, 3);
+    monom mn2(2, 2);
+    monom mn3(3, 1);
+    monom mn4(3, -1);
+    p.addMonom(mn1);
+    p.addMonom(mn2);
+    p1.addMonom(mn3);
+    Polinom res(0);
+    res.addMonom(mn1);
+    res.addMonom(mn2);
+    res.addMonom(mn4);
+    Polinom su(0);
+    su = p - p1;
+    EXPECT_EQ(su, res);
+}
+
+
+
+TEST(Polinom, can_correctly_mult_polinoms)
+{
+    Polinom p(0);
+    Polinom p1(0);
+    monom mn1(1, 2);
+    monom mn2(2, 3);
+    p.addMonom(mn1);
+    p.addMonom(mn2);
+    p1.addMonom(mn1);
+    p1.addMonom(mn2);
+    Polinom res(0);
+    monom mn3(2, 4);
+    monom mn4(3, 12);
+    monom mn5(4, 9);
+    res.addMonom(mn3);
+    res.addMonom(mn4);
+    res.addMonom(mn5);
+    p = p * p1;
+    EXPECT_EQ(p.get_monom(1), res.get_monom(1));
+}
+
+
+TEST(Polynom, can_multiply_by_a_constant) {
+    Polinom p(0);
+    monom mn(2, 1);
+    double a = 3;
+    ASSERT_NO_THROW((const double&)p * a);
+}
+
+TEST(Polynom, can_multiply_by_a_constant_correctly) {
+    Polinom p(0);
+    monom mn(2, 1);
+    p.addMonom(mn);
+    double a = 3.0;
+    Polinom mul(0);
+    mul = p * a;
+    Polinom res(0);
+    monom mn2(2, 3);
+    res.addMonom(mn2);
+    EXPECT_EQ(mul, res);
+}
+
+TEST(Polinom, can_correctly_mult_polinom_and_double)
+{
+    Polinom p(0);
+    double m = 2;
+    monom mn1(1, 2);
+    monom mn2(2, 3);
+    p.addMonom(mn1);
+    p.addMonom(mn2);
+    Polinom res(0);
+    monom mn3(1, 4);
+    monom mn4(2, 6);
+    res.addMonom(mn3);
+    res.addMonom(mn4);
+    p = p * m;
+    EXPECT_EQ(p.get_monom(1), res.get_monom(1));
+}
+
+
+
 
